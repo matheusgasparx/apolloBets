433bce8911b8f10cc02adf20b6ed241e6b5a47ff
gaspar <contatomatheusgaspar@gmail.com>
2024-06-02 17:28:35 -0300

    Create: login forms

--- apollo-vue/src/components/views/Login.vue
+++ apollo-vue/src/components/views/Login.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-      <h3>Login</h3>
+  <div class="container m-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h3>Login</h3>
+          </div>
+          <div class="card-body">
+            <form @submit.prevent="login">
+              <div class="form-group mb-3">
+                <label for="username">Email ou Nome de Usuário</label>
+                <input type="text" class="form-control" id="username" v-model="username" required />
+              </div>
+              <div class="form-group mb-3">
+                <label for="password">Senha</label>
+                <input type="password" class="form-control" id="password" v-model="password" required />
+              </div>
+              <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary btn-block">Entrar</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-      name: 'Login'
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      console.log('Username:', this.username);
+      console.log('Password:', this.password);
+      alert('Login realizado com sucesso! Já pode realizar suas apostas.');
+    }
   }
-  </script>
-  
-  <style>
-  
-  </style>
+};
+</script>
+
+<style scoped>
+.container {
+  margin-top: 50px;
+}
+</style>
